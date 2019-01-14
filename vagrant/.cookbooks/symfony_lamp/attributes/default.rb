2ce71dd84e096f332834880346b2de0256c06a89
Sergey Bondar <Bondar.S.Evg@sberbank.ru>
2019-01-14 15:27:41 +0300

    remove vagrant

--- vagrant/.cookbooks/symfony_lamp/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-default[:app][:server_name]        = "localhost"
-default[:app][:server_aliases]     = ["*.localhost"]
-default[:app][:docroot]            = "/home/vagrant/web-app"
-default[:app][:php_timezone]       = "Europe/Moscow"
